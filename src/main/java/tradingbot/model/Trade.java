7a3884d3d9038e9660869f7c440c999caeaa8fd0
Melih Can AYDIN <melihcanaydin@gmail.com>
2024-12-23 14:50:16 +0200

    Add model classes for Order and Trade entities

---
+++ src/main/java/tradingbot/model/Trade.java
@@ -0,0 +1,60 @@
+package main.java.tradingbot.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Trade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String symbol;
+    private double price;
+    private String action;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    @Override
+    public String toString() {
+        return "Trade{"
+                + "id=" + id
+                + ", symbol='" + symbol + '\''
+                + ", price=" + price
+                + ", action='" + action + '\''
+                + '}';
+    }
+}
