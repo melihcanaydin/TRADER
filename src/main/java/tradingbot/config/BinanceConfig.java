e40bff5a81c5d85f56b3f3ec2324f6ed1e43e647
Melih Can AYDIN <melihcanaydin@gmail.com>
2024-12-24 14:16:15 +0200

    Refactored TradingBotApplication and BinanceConfig for clean code and configuration improvements

--- src/main/java/tradingbot/config/BinanceConfig.java
+++ src/main/java/tradingbot/config/BinanceConfig.java
@@ -1,17 +1,31 @@
-package main.java.tradingbot.config;
+package tradingbot.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.binance.api.client.BinanceApiClientFactory;
 import com.binance.api.client.BinanceApiRestClient;
+import com.binance.api.client.BinanceApiWebSocketClient;
 
 @Configuration
 public class BinanceConfig {
+
+    @Value("${binance.api.key}")
+    private String apiKey;
+
+    @Value("${binance.api.secret}")
+    private String apiSecret;
+
     @Bean
     public BinanceApiRestClient binanceApiRestClient() {
-        BinanceApiClientFactory factory = BinanceApiClientFactory.newInstance(
-                "YOUR_API_KEY", "YOUR_SECRET_KEY");
+        BinanceApiClientFactory factory = BinanceApiClientFactory.newInstance(apiKey, apiSecret);
         return factory.newRestClient();
+    }
+
+    @Bean
+    public BinanceApiWebSocketClient binanceApiWebSocketClient() {
+        BinanceApiClientFactory factory = BinanceApiClientFactory.newInstance(apiKey, apiSecret);
+        return factory.newWebSocketClient();
     }
 }
